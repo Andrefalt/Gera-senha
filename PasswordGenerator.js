cf42959a6440add471e9fc15c30b812fc16cda43
Sergio Cabral <git@sergiocabral.com>
2020-12-18 22:36:48 -0300

    Classe PasswordGeneratorConfig para agrupar configurações.

--- PasswordGenerator.js
+++ PasswordGenerator.js
@@ -1,3 +1,21 @@
+class PasswordGeneratorConfig {
+
+    constructor(
+        length,
+        useLowercaseLetters,
+        useUppercaseLetters,
+        useNumbers,
+        useSymbols) {
+
+        this.length = Number.isFinite(length) ? Number(length) : 20;
+        this.useLowercaseLetters = Boolean(useLowercaseLetters);
+        this.useUppercaseLetters = Boolean(useUppercaseLetters);
+        this.useNumbers = Boolean(useNumbers);
+        this.useSymbols = Boolean(useSymbols);
+    }
+
+}
+
 class PasswordGenerator {
 
     run() {
